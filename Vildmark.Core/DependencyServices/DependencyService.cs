eabf1e0e67c596aee8c307eb3b762a239e669377
Philippe Paré <phyyl@live.ca>
2020-06-07 23:53:34 -0400

    Added Global dependency service for convenience

--- Vildmark.Core/DependencyServices/DependencyService.cs
+++ Vildmark.Core/DependencyServices/DependencyService.cs
@@ -8,6 +8,17 @@ namespace Vildmark.DependencyServices
 {
 	public class DependencyService : IDependencyService
 	{
+		public static IDependencyService Global { get; set; } = new DependencyService();
+
+		static DependencyService()
+		{
+			IDependencyServiceAssemblyProvider assemblyProvider = new AppDomainDependencyServiceAssemblyProvider();
+			IDependencyServiceTypeProvider provider = new AttributeDependencyServiceTypeProvider(assemblyProvider);
+			IDependencyServiceTypeRegistrer registrer = new CascadingDependencyServiceTypeRegistrer(Global, provider);
+
+			registrer.RegisterServices();
+		}
+
 		private readonly Dictionary<Type, object> services = new Dictionary<Type, object>();
 
 		public DependencyService()
