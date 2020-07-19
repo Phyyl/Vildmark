dcbe330168ede0af8cf879844500201adebbc3cf
Philippe Paré <phyyl@live.ca>
2020-07-18 23:25:02 -0400

    Added texCoordRotation option to CubeHelper

--- Vildmark.Graphics/Helpers/CubeHelper.cs
+++ Vildmark.Graphics/Helpers/CubeHelper.cs
@@ -6,94 +6,112 @@ namespace Vildmark.Graphics.Helpers
 {
 	public static class CubeHelper
 	{
-		public static IEnumerable<Vertex> GetRightVertices() => GetRightVertices(Vector3.Zero, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetRightVertices(Vector4 color) => GetRightVertices(Vector3.Zero, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetRightVertices(Vector3 offset) => GetRightVertices(offset, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetRightVertices(Vector3 offset, Vector4 color) => GetRightVertices(offset, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetRightVertices(Vector3 offset, Vector3 size) => GetRightVertices(offset, size, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetRightVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize)
+		public static IEnumerable<Vertex> GetRightVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize, int texCoordRotation = 0)
 		{
-			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(1, 0, 0));
-			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(1, 0, 0));
-			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(1, 0, 0));
-			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(1, 0, 0));
-			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(1, 0, 0));
-			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(1, 0, 0));
+			Vector2[] texCoords = new Vector2[]
+			{
+				texCoord + new Vector2(0, 0) * texSize,
+				texCoord + new Vector2(0, 1) * texSize,
+				texCoord + new Vector2(1, 1) * texSize,
+				texCoord + new Vector2(1, 0) * texSize
+			};
+
+			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(1, 0, 0));
+			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoords[(1 + texCoordRotation) % texCoords.Length], color, new Vector3(1, 0, 0));
+			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(1, 0, 0));
+			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(1, 0, 0));
+			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(1, 0, 0));
+			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoords[(3 + texCoordRotation) % texCoords.Length], color, new Vector3(1, 0, 0));
 		}
 
-		public static IEnumerable<Vertex> GetLeftVertices() => GetLeftVertices(Vector3.Zero, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetLeftVertices(Vector4 color) => GetLeftVertices(Vector3.Zero, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetLeftVertices(Vector3 offset) => GetLeftVertices(offset, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetLeftVertices(Vector3 offset, Vector4 color) => GetLeftVertices(offset, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetLeftVertices(Vector3 offset, Vector3 size) => GetLeftVertices(offset, size, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetLeftVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize)
+		public static IEnumerable<Vertex> GetLeftVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize, int texCoordRotation = 0)
 		{
-			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(-1, 0, 0));
-			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(-1, 0, 0));
-			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(-1, 0, 0));
-			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(-1, 0, 0));
-			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(-1, 0, 0));
-			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(-1, 0, 0));
+			Vector2[] texCoords = new Vector2[]
+			{
+				texCoord + new Vector2(0, 0) * texSize,
+				texCoord + new Vector2(0, 1) * texSize,
+				texCoord + new Vector2(1, 1) * texSize,
+				texCoord + new Vector2(1, 0) * texSize
+			};
+
+			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(-1, 0, 0));
+			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoords[(1 + texCoordRotation) % texCoords.Length], color, new Vector3(-1, 0, 0));
+			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(-1, 0, 0));
+			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(-1, 0, 0));
+			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(-1, 0, 0));
+			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoords[(3 + texCoordRotation) % texCoords.Length], color, new Vector3(-1, 0, 0));
 		}
 
-		public static IEnumerable<Vertex> GetTopVertices() => GetTopVertices(Vector3.Zero, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetTopVertices(Vector4 color) => GetTopVertices(Vector3.Zero, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetTopVertices(Vector3 offset) => GetTopVertices(offset, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetTopVertices(Vector3 offset, Vector4 color) => GetTopVertices(offset, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetTopVertices(Vector3 offset, Vector3 size) => GetTopVertices(offset, size, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetTopVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize)
+		public static IEnumerable<Vertex> GetTopVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize, int texCoordRotation = 0)
 		{
-			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, 1, 0));
-			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(0, 1, 0));
-			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, 1, 0));
-			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, 1, 0));
-			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, 1, 0));
-			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(0, 1, 0));
+			Vector2[] texCoords = new Vector2[]
+			{
+				texCoord + new Vector2(0, 0) * texSize,
+				texCoord + new Vector2(0, 1) * texSize,
+				texCoord + new Vector2(1, 1) * texSize,
+				texCoord + new Vector2(1, 0) * texSize
+			};
+
+			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 1, 0));
+			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoords[(1 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 1, 0));
+			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 1, 0));
+			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 1, 0));
+			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 1, 0));
+			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoords[(3 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 1, 0));
 		}
 
-		public static IEnumerable<Vertex> GetBottomVertices() => GetBottomVertices(Vector3.Zero, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBottomVertices(Vector4 color) => GetBottomVertices(Vector3.Zero, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBottomVertices(Vector3 offset) => GetBottomVertices(offset, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBottomVertices(Vector3 offset, Vector4 color) => GetBottomVertices(offset, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBottomVertices(Vector3 offset, Vector3 size) => GetBottomVertices(offset, size, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBottomVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize)
+		public static IEnumerable<Vertex> GetBottomVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize, int texCoordRotation = 0)
 		{
-			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, -1, 0));
-			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(0, -1, 0));
-			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, -1, 0));
-			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, -1, 0));
-			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, -1, 0));
-			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(0, -1, 0));
+			Vector2[] texCoords = new Vector2[]
+			{
+				texCoord + new Vector2(0, 0) * texSize,
+				texCoord + new Vector2(0, 1) * texSize,
+				texCoord + new Vector2(1, 1) * texSize,
+				texCoord + new Vector2(1, 0) * texSize
+			};
+
+			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, -1, 0));
+			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoords[(1 + texCoordRotation) % texCoords.Length], color, new Vector3(0, -1, 0));
+			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, -1, 0));
+			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, -1, 0));
+			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, -1, 0));
+			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoords[(3 + texCoordRotation) % texCoords.Length], color, new Vector3(0, -1, 0));
 		}
 
-		public static IEnumerable<Vertex> GetFrontVertices() => GetFrontVertices(Vector3.Zero, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetFrontVertices(Vector4 color) => GetFrontVertices(Vector3.Zero, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetFrontVertices(Vector3 offset) => GetFrontVertices(offset, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetFrontVertices(Vector3 offset, Vector4 color) => GetFrontVertices(offset, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetFrontVertices(Vector3 offset, Vector3 size) => GetFrontVertices(offset, size, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetFrontVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize)
+		public static IEnumerable<Vertex> GetFrontVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize, int texCoordRotation = 0)
 		{
-			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, 0, 1));
-			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(0, 0, 1));
-			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, 0, 1));
-			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, 0, 1));
-			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, 0, 1));
-			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(0, 0, 1));
+			Vector2[] texCoords = new Vector2[]
+			{
+				texCoord + new Vector2(0, 0) * texSize,
+				texCoord + new Vector2(0, 1) * texSize,
+				texCoord + new Vector2(1, 1) * texSize,
+				texCoord + new Vector2(1, 0) * texSize
+			};
+
+			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, 1));
+			yield return new Vertex(new Vector3(0, 0, 1) * size + offset, texCoords[(1 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, 1));
+			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, 1));
+			yield return new Vertex(new Vector3(0, 1, 1) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, 1));
+			yield return new Vertex(new Vector3(1, 0, 1) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, 1));
+			yield return new Vertex(new Vector3(1, 1, 1) * size + offset, texCoords[(3 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, 1));
 		}
 
-		public static IEnumerable<Vertex> GetBackVertices() => GetBackVertices(Vector3.Zero, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBackVertices(Vector4 color) => GetBackVertices(Vector3.Zero, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBackVertices(Vector3 offset) => GetBackVertices(offset, Vector3.One, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBackVertices(Vector3 offset, Vector4 color) => GetBackVertices(offset, Vector3.One, color, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBackVertices(Vector3 offset, Vector3 size) => GetBackVertices(offset, size, Vector4.One, Vector2.Zero, Vector2.One);
-		public static IEnumerable<Vertex> GetBackVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize)
+		public static IEnumerable<Vertex> GetBackVertices(Vector3 offset, Vector3 size, Vector4 color, Vector2 texCoord, Vector2 texSize, int texCoordRotation = 0)
 		{
-			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, 0, -1));
-			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoord + new Vector2(0, 1) * texSize, color, new Vector3(0, 0, -1));
-			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, 0, -1));
-			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoord + new Vector2(0, 0) * texSize, color, new Vector3(0, 0, -1));
-			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoord + new Vector2(1, 1) * texSize, color, new Vector3(0, 0, -1));
-			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoord + new Vector2(1, 0) * texSize, color, new Vector3(0, 0, -1));
+			Vector2[] texCoords = new Vector2[]
+			{
+				texCoord + new Vector2(0, 0) * texSize,
+				texCoord + new Vector2(0, 1) * texSize,
+				texCoord + new Vector2(1, 1) * texSize,
+				texCoord + new Vector2(1, 0) * texSize
+			};
+
+			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, -1));
+			yield return new Vertex(new Vector3(1, 0, 0) * size + offset, texCoords[(1 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, -1));
+			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, -1));
+			yield return new Vertex(new Vector3(1, 1, 0) * size + offset, texCoords[(0 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, -1));
+			yield return new Vertex(new Vector3(0, 0, 0) * size + offset, texCoords[(2 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, -1));
+			yield return new Vertex(new Vector3(0, 1, 0) * size + offset, texCoords[(3 + texCoordRotation) % texCoords.Length], color, new Vector3(0, 0, -1));
 		}
 	}
 }
