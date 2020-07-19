465ee289aece3804bb01512a7b6ba71d3bb38a25
Philippe Paré <phyyl@live.ca>
2020-07-19 03:34:40 -0400

    Added TimedCoroutineRunner

---
+++ Vildmark.Core/Coroutines/TimedCoroutineRunner.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Vildmark.Coroutines
+{
+	public class TimedCoroutineRunner
+	{
+		private readonly int ms;
+		private readonly ICoroutine coroutine;
+		private IEnumerator enumerator;
+
+		public bool Done { get; private set; }
+
+		public TimedCoroutineRunner(int ms, ICoroutine coroutine)
+		{
+			this.ms = ms;
+			this.coroutine = coroutine;
+		}
+
+		public int Update()
+		{
+			if (Done)
+			{
+				return 0;
+			}
+
+			int updates = 0;
+
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			while (stopwatch.ElapsedMilliseconds < ms)
+			{
+				if (enumerator is null)
+				{
+					enumerator = coroutine.Run().GetEnumerator();
+					updates++;
+				}
+				else
+				{
+					if (!enumerator.MoveNext())
+					{
+						Done = true;
+						break;
+					}
+
+					updates++;
+				}
+			}
+
+			return updates;
+		}
+	}
+}
