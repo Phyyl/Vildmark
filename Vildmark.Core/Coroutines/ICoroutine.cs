dcadfba2427a0532e8379c5e4448dd9d1526b763
Philippe Paré <phyyl@live.ca>
2020-07-19 03:33:52 -0400

    Added ICoroutine

---
+++ Vildmark.Core/Coroutines/ICoroutine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vildmark.Coroutines
+{
+	public interface ICoroutine
+	{
+		IEnumerable Run();
+	}
+}
