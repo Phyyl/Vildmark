d7b11cc9236d50b61cf0887d3a8172a54e657c59
Philippe Paré <phyyl@live.ca>
2020-09-25 20:22:50 -0400

    Added scale to cameras

--- Vildmark.Graphics/Cameras/Camera.cs
+++ Vildmark.Graphics/Cameras/Camera.cs
@@ -5,11 +5,14 @@ namespace Vildmark.Graphics.Cameras
 {
     public abstract class Camera
     {
-        private Vector3 translate;
+        private Vector3 translation;
         private Vector3 rotation;
+        private float scale;
 
-        public ref Vector3 Translation => ref translate;
+        public ref Vector3 Translation => ref translation;
         public ref Vector3 Rotation => ref rotation;
+
+        public ref float Scale => ref scale;
 
         public abstract Matrix4 ProjectionMatrix { get; }
 
@@ -17,21 +20,26 @@ namespace Vildmark.Graphics.Cameras
         {
             get
             {
-                Matrix4 matrix = translate.LengthSquared > 0 ? Matrix4.CreateTranslation(translate) : Matrix4.Identity;
+                Matrix4 matrix = translation.LengthSquared > 0 ? Matrix4.CreateTranslation(translation) : Matrix4.Identity;
+
+                if (scale != 1)
+                {
+                    matrix *= Matrix4.CreateScale(scale);
+                }
 
                 if (rotation.Y > 0)
                 {
-                    matrix *= Matrix4.CreateRotationY(Rotation.Y);
+                    matrix *= Matrix4.CreateRotationY(rotation.Y);
                 }
 
                 if (rotation.X > 0)
                 {
-                    matrix *= Matrix4.CreateRotationY(Rotation.X);
+                    matrix *= Matrix4.CreateRotationY(rotation.X);
                 }
 
                 if (rotation.Z > 0)
                 {
-                    matrix *= Matrix4.CreateRotationY(Rotation.Z);
+                    matrix *= Matrix4.CreateRotationY(rotation.Z);
                 }
 
                 return matrix;
