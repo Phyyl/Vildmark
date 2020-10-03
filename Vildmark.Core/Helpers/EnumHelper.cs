9dd25cd54417b0bce0ba0f988f5c76539c129b4d
Philippe Paré <phyyl@live.ca>
2020-10-03 01:33:17 -0400

    Added EnumHelper

---
+++ Vildmark.Core/Helpers/EnumHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Vildmark.Helpers
+{
+    public static class EnumHelper
+    {
+        public static void ForEach<T>(Action<T> action) where T : Enum
+        {
+            foreach (var value in GetValues<T>())
+            {
+                action(value);
+            }
+        }
+
+        public static IEnumerable<T> GetValues<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
