8f8234d0facef9b3a043554a4c1239b89381230f
Philippe Paré <phyyl@live.ca>
2022-01-06 12:09:04 -0500

    Fixed TextureAtlas source rectangle too big

--- Vildmark.Graphics/Rendering/TextureAtlas.cs
+++ Vildmark.Graphics/Rendering/TextureAtlas.cs
@@ -5,8 +5,6 @@ namespace Vildmark.Graphics.Rendering
 {
     public class TextureAtlas
     {
-        public Vector2 TextureTileSize { get; }
-
         public Vector2 TileSize { get; }
 
         public Texture2D Texture { get; }
@@ -14,11 +12,9 @@ namespace Vildmark.Graphics.Rendering
         public TextureAtlas(Texture2D texture, int tileWidth, int tileHeight)
         {
             Texture = texture;
-
             TileSize = new Vector2(tileWidth, tileHeight);
-            TextureTileSize = new Vector2(tileWidth / (float)texture.Width, tileHeight / (float)texture.Height);
         }
 
-        public Texture2D this[int x, int y] => Texture.CreateSubTexture(new RectangleF(x * TextureTileSize.X, y * TextureTileSize.Y, TextureTileSize.X, TextureTileSize.Y));
+        public Texture2D this[int x, int y] => Texture.CreateSubTexture(new RectangleF(x / Texture.Width, y / Texture.Height, TileSize.X / Texture.Width, TileSize.Y / Texture.Height));
     }
 }
