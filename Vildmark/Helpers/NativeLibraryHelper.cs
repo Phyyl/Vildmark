312f5a26b26f9b797376401a36be25d306914ed9
Philippe Paré <phyyl@live.ca>
2022-01-28 18:58:10 -0500

    Fixed OpenAL loading

--- Vildmark/Helpers/NativeLibraryHelper.cs
+++ Vildmark/Helpers/NativeLibraryHelper.cs
@@ -4,10 +4,10 @@ namespace Vildmark.Helpers
 {
     internal static class NativeLibraryHelper
     {
-        public static IntPtr LoadNativeLibrary(string path)
+        public static IntPtr LoadNativeLibrary(string name)
         {
-            string osFolder = OperatingSystem.IsLinux() ? "linux" : "windows";
-            string loadPath = $@"libs\{osFolder}\{path}";
+            string osFolder = OperatingSystem.IsLinux() ? "linux-x64" : "win-x64";
+            string loadPath = $@"runtimes\{osFolder}\native\{name}";
 
             return NativeLibrary.Load(loadPath);
         }
