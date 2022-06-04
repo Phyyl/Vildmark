4ab1c7c4f31b2dce572daced7889f8ad6b7bbbd8
Philippe Paré <phyyl@live.ca>
2022-06-04 02:31:11 -0400

    Added RenderString extension

--- Vildmark/Graphics/Textures/Loaders/Texture2DResourceLoader.cs
+++ Vildmark/Graphics/Textures/Loaders/Texture2DResourceLoader.cs
@@ -9,14 +9,11 @@ internal class Texture2DResourceLoader : IResourceLoader<Texture2D>
 {
     public unsafe Texture2D Load(string name, ResourceLoadContext context)
     {
-#pragma warning disable CA1416 // Validate platform compatibility
         Bitmap bitmap = new(context.GetStream(name));
         BitmapData data = bitmap.LockBits(new Rectangle(Point.Empty, bitmap.Size), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
         Span<byte> span = new(data.Scan0.ToPointer(), bitmap.Width * 4 * bitmap.Height);
         GLTexture2D texture = new(bitmap.Width, bitmap.Height, span);
         bitmap.UnlockBits(data);
-#pragma warning restore CA1416 // Validate platform compatibility
-
         return new Texture2D(texture);
     }
 }
