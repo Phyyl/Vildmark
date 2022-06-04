b7cea8c797b3c5188954cfe5a28eb88c368e88df
Philippe Paré <phyyl@live.ca>
2022-06-04 02:31:11 -0400

    Added RenderString extension

---
+++ Vildmark/Extensions.Rendering.cs
@@ -0,0 +1,14 @@
+﻿using OpenTK.Mathematics;
+using Vildmark.Graphics;
+using Vildmark.Graphics.Fonts.Msdf;
+using Vildmark.Graphics.Rendering;
+
+namespace Vildmark;
+
+public static partial class Extensions
+{
+    public static void RenderString(this Renderer renderer, MsdfFont font, string text, float size, Color4 foreground, Color4? background = default, Transform? transform = default)
+    {
+        font.RenderString(renderer, text, size, foreground, background, transform);
+    }
+}
