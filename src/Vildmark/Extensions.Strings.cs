7737018c75dcd822d1a8de645d93ab544f0cc608
Philippe Paré <phyyl@live.ca>
2022-06-04 12:51:39 -0400

    Added control characters to text output

---
+++ src/Vildmark/Extensions.Strings.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace Vildmark;
+
+public static partial class Extensions
+{
+    public static string EscapeControlCharacters(this string str)
+    {
+        StringBuilder stringBuilder = new();
+
+        foreach (var chr in str)
+        {
+            stringBuilder.Append(chr switch
+            {
+                '\0' => "\\0",
+                '\a' => "\\a",
+                '\b' => "\\b",
+                '\f' => "\\f",
+                '\n' => "\\n",
+                '\r' => "\\r",
+                '\t' => "\\t",
+                '\v' => "\\v",
+                _ => chr
+            });
+        }
+
+        return stringBuilder.ToString();
+    }
+}
