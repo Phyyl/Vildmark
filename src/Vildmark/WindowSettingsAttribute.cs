1460d7ffee932700151cb91f5838aa267b2f256c
Philippe Paré <phyyl@live.ca>
2022-06-07 00:58:07 -0400

    Added missing WindowSettingsAttribute

---
+++ src/Vildmark/WindowSettingsAttribute.cs
@@ -0,0 +1,16 @@
+﻿using OpenTK.Windowing.Common;
+
+namespace Vildmark;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class WindowSettingsAttribute : Attribute
+{
+    public string Title { get; init; } = "Game";
+
+    public int Width { get; init; } = 1280;
+    public int Height { get; init; } = 720;
+    public int Samples { get; init; } = 0;
+
+    public WindowState State { get; init; } = WindowState.Normal;
+    public WindowBorder Border { get; init; } = WindowBorder.Resizable;
+}
