d9906c87c39a3dbdd8cef004e0eba69fbef2805e
Philippe Paré <phyyl@live.ca>
2022-06-07 23:22:19 -0400

    Renamed QubicleMatrixModel to QubicleModel

--- src/Vildmark/Qubicle/QubicleModel.cs
+++ src/Vildmark/Qubicle/QubicleModel.cs
@@ -2,15 +2,15 @@ using Vildmark.Resources;
 
 namespace Vildmark.Qubicle;
 
-[ResourceLoader(typeof(QubicleMatrixCollection))]
-public class QubicleMatrixCollection
+[ResourceLoader(typeof(QubicleModelResourceLoader))]
+public class QubicleModel
 {
     private readonly Dictionary<string, QubicleMatrix> matrices;
 
     public IEnumerable<QubicleMatrix> Matrices => matrices.Values.ToArray();
     public QubicleMatrix? this[string name] => matrices.GetValueOrDefault(name);
 
-    public QubicleMatrixCollection(IEnumerable<QubicleMatrix> matrices)
+    public QubicleModel(IEnumerable<QubicleMatrix> matrices)
     {
         this.matrices = matrices.ToDictionary(m => m.Name);
     }
