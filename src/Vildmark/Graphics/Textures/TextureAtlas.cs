d9af907edebb1f1a8146773dc7b83121ffa86428
Philippe Paré <phyyl@live.ca>
2022-07-30 13:14:10 -0400

    Added texture atlas spacing option

--- src/Vildmark/Graphics/Textures/TextureAtlas.cs
+++ src/Vildmark/Graphics/Textures/TextureAtlas.cs
@@ -4,23 +4,31 @@ namespace Vildmark.Graphics.Textures;
 
 public class TextureAtlas
 {
-    public Vector2 TileSize { get; }
+    public Vector2 TexelTileSize { get; }
+    public Vector2 TexelTileSpacing { get; }
+
+    public Vector2i TileSize { get; }
+    public Vector2i TileSpacing { get; }
 
     public Texture2D Texture { get; }
 
-    public TextureAtlas(Texture2D texture, int tileWidth, int tileHeight)
+    public TextureAtlas(Texture2D texture, int tileWidth, int tileHeight, int xSpacing = 0, int ySpacing = 0)
     {
         Texture = texture;
-        TileSize = new Vector2(tileWidth, tileHeight) / texture.Size;
+        TileSize = new(tileWidth, tileHeight);
+        TileSpacing = new Vector2i(xSpacing, ySpacing);
+
+        TexelTileSize = TileSize / texture.Size;
+        TexelTileSpacing = TileSpacing / texture.Size;
     }
 
     public SubTexture2D this[int x, int y]
     {
         get
         {
-            Vector2 min = new Vector2(x, y) * TileSize;
-            
-            return new SubTexture2D(Texture, new Box2(min, min + TileSize));
+            Vector2 min = new Vector2(x, y) * (TexelTileSize + TexelTileSpacing);
+
+            return new SubTexture2D(Texture, new Box2(min, min + TexelTileSize));
         }
     }
 }
