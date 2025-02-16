b980a1be22c87a070540bad169203470c5f0c905
Philippe Paré <phyyl@live.ca>
2025-02-15 19:25:49 -0500

    Fixed texture scaling in TextureDictionary

--- src/Vildmark/Graphics/Textures/TextureDictionary.cs
+++ src/Vildmark/Graphics/Textures/TextureDictionary.cs
@@ -2,14 +2,24 @@ using OpenTK.Mathematics;
 
 namespace Vildmark.Graphics.Textures;
 
+public class TextureDictionary(Texture2D texture, params TextureDictionary<string>.Entry[] entries) : TextureDictionary<string>(texture, entries);
+
 public class TextureDictionary<TKey>(Texture2D texture, params TextureDictionary<TKey>.Entry[] entries)
     where TKey : notnull
 {
-    private readonly Dictionary<TKey, SubTexture2D> rectangles = entries.ToDictionary(e => e.Key, e => new SubTexture2D(texture, e.SourceRectangle));
+    private readonly Dictionary<TKey, SubTexture2D> rectangles = entries.ToDictionary(
+        e => e.Key,
+        e => new SubTexture2D(
+            texture,
+            new Box2(
+                e.Region.Min.X / texture.Width,
+                e.Region.Min.Y / texture.Height,
+                e.Region.Max.X / texture.Width,
+                e.Region.Max.Y / texture.Height)));
 
     internal Texture2D Texture { get; } = texture;
 
     public Texture2D? this[TKey key] => rectangles.GetValueOrDefault(key);
 
-    public record Entry(TKey Key, Box2 SourceRectangle);
+    public record Entry(TKey Key, Box2 Region);
 }
